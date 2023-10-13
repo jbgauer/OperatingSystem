507cc0f87d346177098d048873d8adf79b50015d
Gauer <jackbg2@illinois.edu>
2023-10-13 14:55:57 -0500

    pic file written

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -5,60 +5,66 @@
 #include "i8259.h"
 #include "lib.h"
 
-/*Port Values for PIC*/
-uint8_t irq_mask = 0xFFFF;   /*Mask for both PICs*/
-
 /* Interrupt masks to determine which interrupts are enabled and disabled */
-uint8_t master_mask = irq_mask; /* IRQs 0-7  */
-uint8_t slave_mask = irq_mask >> 8;  /* IRQs 8-15 */
+uint8_t master_mask; /* IRQs 0-7  */
+uint8_t slave_mask; /* IRQs 8-15 */
 
 
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
-    /*mask all of master PIC*/
-    //outb(0xFF, MASTER_8259_PORT);
-    /*mask all of slave PIC*/
-   // outb(0xFF, SLAVE_8259_PORT);
-    /*write initialization control words*/
-    //outb(ICW1, /*?*/);
-   // outb(ICW2_MASTER, );
-   // outb(ICW3_MASTER, );
-    //outb(ICW4, /*?*/)
 
+    /*set interupt masks*/
+    master_mask = inb(MASTER_8259_IMR);
+    slave_mask = inb(SLAVE_8259_IMR);
+    /*mask all of master/slave PIC*/
+    outb(0xFF, MASTER_8259_IMR);
+    outb(0xFF, SLAVE_8259_IMR);
+
+    /*master PIC control words*/
+    outb(ICW1, MASTER_8259_PORT);
+    outb(ICW2_MASTER, MASTER_8259_PORT);
+    outb(ICW3_MASTER, MASTER_8259_PORT);
+    outb(ICW4, MASTER_8259_PORT);
+    
     /*slave PIC control words*/
-    //outb(ICW1, /*?*/);
-   // outb(ICW2_SLAVE, );
-   // outb(ICW3_SLAVE, );
-    //outb(ICW4, /*?*/)
-   // outb(master_mask, MASTER8259_PORT);
-  //  outb(slave_mask, SLAVE_8259_PORT);
+    outb(ICW1, SLAVE_8259_PORT);
+    outb(ICW2_SLAVE, SLAVE_8259_PORT);
+    outb(ICW3_SLAVE, SLAVE_8259_PORT);
+    outb(ICW4, SLAVE_8259_PORT);
+    /*restore PICs*/
+    outb(master_mask, MASTER_8259_PORT);
+    outb(slave_mask, MASTER_8259_PORT);
+    
+
 }
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
-    uint8_t mask = ~(1 << irq_num);
-    irq_mask &= mask;
     /*Master or Slave PIC?*/
     if(irq_num & 8) {
         /*Slave PIC*/
+        irq_num -= 8;
+        slave_mask = inb(SLAVE_8259_PORT) & ~(1 << irq_num);
         outb(slave_mask, SLAVE_8259_PORT);
     } else {
         /*Master PIC*/
+        master_mask = inb(MASTER_8259_PORT) & ~(1 << irq_num);
         outb(master_mask,MASTER_8259_PORT);
     }
 }
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
-    uint8_t mask = 1 << irq_num;
-    irq_mask |= mask;
     /*Master or Slave PIC?*/
     if(irq_num & 8) {
         /*Slave PIC*/
+        irq_num -= 8;
+        slave_mask = inb(SLAVE_8259_PORT) | (1 << irq_num);
         outb(slave_mask, SLAVE_8259_PORT);
     } else {
         /*Master PIC*/
+        master_mask = inb(MASTER_8259_PORT) | (1 << irq_num);
         outb(master_mask,MASTER_8259_PORT);
     }
 }
