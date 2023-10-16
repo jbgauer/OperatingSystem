c23c36f6ade10cc4d5d75b853466e7f5bbe9e1dc
Blade <bblade2@illinois.edu>
2023-10-16 17:32:26 -0500

    Checkpoint 1 FINAL!

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -45,12 +45,30 @@ int idt_test(){
 	return result;
 }
 
+/* divideByZero_test
+ * 
+ * Tests divide by zero exception
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: divide by zero exception
+ * Files: idt.c/h
+ */
 void divideByZero_test(){
 	int i;
 	int j = 0;
 	i = 1/j;
 }
 
+/* bound_range_exceeded_test
+ * 
+ * Tests bound range exception
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: bound range exception
+ * Files: handlerWrap.S/h
+ */
 void bound_range_exceeded_test(){
 	// unsigned int i[1];
 	// i[0] = 5;
@@ -58,18 +76,100 @@ void bound_range_exceeded_test(){
 	asm("INT $0x05");
 }
 
+/* syscall_test
+ * 
+ * Tests system call is loaded in idt
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: system calls in idt
+ * Files: idt.c/h
+ */
 void syscall_test(){
 	asm("INT $0x80");                                  
 }
 
+/* rtc_test
+ * 
+ * Tests that rtc interrupt happens
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: rtc device interrupts
+ * Files: idt.c/h i8259.c/h
+ */
 void rtc_test(){
 	asm("INT $0x28");
 }
-/*
-void deref_test() {
-	int
+
+/* deref_test_zero
+ * 
+ * Tests memory location 0 is null
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: memory location 0
+ * Files: paging.c/h
+ */
+void deref_test_zero() {
+	unsigned* a;
+	unsigned b;
+	a = (unsigned*)0;
+	b = *a;
+	printf("%x", b);
 }
-*/
+
+/* deref_test_kernel
+ * 
+ * Tests memory location at start of kernel isn't null
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: memory location 0x00400000
+ * Files: paging.c/h
+ */
+void deref_test_kernel() {
+	unsigned* a;
+	unsigned b;
+	a = (unsigned*)0x00400000;
+	b = *a;
+	printf("%x", b);
+}
+
+/* deref_test_after_kernel
+ * 
+ * Tests memory location right after kernel is null
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: memory location 0x00800000
+ * Files: paging.c/h
+ */
+void deref_test_after_kernel() {
+	unsigned* a;
+	unsigned b;
+	a = (unsigned*)0x00800000;
+	b = *a;
+	printf("%x", b);
+}
+
+/* deref_test_vid_mem
+ * 
+ * Tests memory location at start of video memory isn't null
+ * Inputs: None
+ * Outputs: PASS/FAIL
+ * Side Effects: None
+ * Coverage: memory location 0xB8000
+ * Files: paging.c/h
+ */
+void deref_test_vid_mem() {
+	unsigned* a;
+	unsigned b;
+	a = (unsigned*)0xB8000;
+	b = *a;
+	printf("%x", b);
+}
+
 
 // add more tests here
 
@@ -87,6 +187,8 @@ void launch_tests(){
 	// bound_range_exceeded_test();
 	// syscall_test();
 	// rtc_test();
-	
-
+	// deref_test_zero();
+	// deref_test_vid_mem();
+	// deref_test_kernel();
+	// deref_test_after_kernel();
 }
