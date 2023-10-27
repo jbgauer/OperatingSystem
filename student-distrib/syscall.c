b903fb7f2201a3fdb95eec8f363ff3da6211285a
Gauer <jackbg2@illinois.edu>
2023-10-27 14:08:24 -0500

    syscall file fixes

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -1,179 +1,179 @@
-// #include "syscall.h"
-// #include "lib.h"
+#include "syscall.h"
 
 
-// /*
-//  * halt
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t halt (uint8_t status) {
 
-// }
+/*
+ * halt
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t halt (uint8_t status) {
+
+}
 
 
-// /*
-//  * system_execute
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t
-// execute(const uint8_t *command) {
+/*
+ * system_execute
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t
+execute(const uint8_t *command) {
         
-//     uint8_t *cmdHold;
-//     uint8_t *cmdArgs;
-//     uint8_t *file;
-//     uint32_t i;
+    uint8_t *cmdHold;
+    uint8_t *cmdArgs;
+    uint8_t *file;
+    uint32_t i;
 
-//     if(command == NULL) {
-//         return -1;
-//     }
-//     /*Parse Args*/
-//     cmdHold = command;
-//     //skip spaces
-//     for(int i = 0; i < COMMAND_MAX; i++) {
-//         if(cmdHold == ' ') {
-//             cmdHold++;
-//         } else { 
-//             break;
-//         }
-//     } 
-//     file = cmdHold;
-//     //check for args
-//     for(int j = 0; j = COMMAND_MAX-i; j++) {
-//         if(cmdHold == ' ' || cmdHold == '\n' || cmdHold == '\0') {
-//             cmdHold++;
-//             break;
-//         }
-//         cmdHold++;
-//     }
-//     cmdArgs = cmdHold;
+    if(command == NULL) {
+        return -1;
+    }
+    /*Parse Args*/
+    cmdHold = command;
+    //skip spaces
+    for(int i = 0; i < COMMAND_MAX; i++) {
+        if(cmdHold == ' ') {
+            cmdHold++;
+        } else { 
+            break;
+        }
+    } 
+    file = cmdHold;
+    //check for args
+    for(int j = 0; j = COMMAND_MAX-i; j++) {
+        if(cmdHold == ' ' || cmdHold == '\n' || cmdHold == '\0') {
+            cmdHold++;
+            break;
+        }
+        cmdHold++;
+    }
+    cmdArgs = cmdHold;
     
-//     /*Executable Check*/
-//     //file open check if return -1
-//     //read data check if return -1
-//     //file type
-//     //if 0x464C457F
+    /*Executable Check*/
+    //file open check if return -1
+    //read data check if return -1
+    //file type
+    //if 0x464C457F
 
 
-//     /*set up program paging*/
-
-
-
-//     /*user-level program loader*/
-//     //sys all in eax, first arg in EBX, ECX, EDX
-
-//     /*create pcb*/
-
-//     /*context switch (IN x86)*/
-//         //create its own context switch stack
-//         //IRET
-
-// }
+    /*set up program paging*/
 
 
 
-// /*
-//  * read
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t read (nt32_t fd, void* buf, nit32_t nbytes) {
+    /*user-level program loader*/
+    //sys all in eax, first arg in EBX, ECX, EDX
 
-// }
+    /*create pcb*/
 
-// /*
-//  * write
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t write (int32_t fd, const void* buf, int32_t nbytes) {
+    /*context switch (IN x86)*/
+        //create its own context switch stack
+        //IRET
 
-// }
+}
 
-// /*
-//  * open
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t open (const uint8_t* filename) {
 
-// }
 
-// /*
-//  * close
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t close (int32_t fd) {
+/*
+ * read
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t read (int32_t fd, void* buf, int32_t nbytes); {
 
-// }
+}
 
-// /*
-//  * getargs
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t getargs (uint8_t* buf, int32_t nbytes) {
+/*
+ * write
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t write (int32_t fd, const void* buf, int32_t nbytes) {
 
-// }
+}
 
-// /*
-//  * vidmap
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t vidmap (uint8_t** screen_start) {
+/*
+ * open
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t open (const uint8_t* filename) {
 
-// }
+}
 
-// /*
-//  * set_handler
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t set_handler(int32_t signum, void* handler_address) {
+/*
+ * close
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t close (int32_t fd) {
 
-// }
+}
 
-// /*
-//  * sigreturn
-//  *   DESCRIPTION: 
-//  *   INPUTS:
-//  *   OUTPUTS: none
-//  *   RETURN VALUE: 
-//  *   SIDE EFFECTS: none
-//  */
-// int32_t sigreturn (void) {
+/*
+ * getargs
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t getargs (uint8_t* buf, int32_t nbytes) {
 
-// }
+}
+
+/*
+ * vidmap
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t vidmap (uint8_t** screen_start) {
+
+}
+
+/*
+ * set_handler
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t set_handler(int32_t signum, void* handler_address) {
+
+}
+
+/*
+ * sigreturn
+ *   DESCRIPTION: 
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: 
+ *   SIDE EFFECTS: none
+ */
+int32_t sigreturn (void) {
+
+}
 
 
 
--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -1,28 +1,29 @@
 
-// #ifndef _EXECUTE_H
-// #define _EXECUTE_H
+#ifndef _SYSCALL_H
+#define _SYSCALL_H
 
+#include "lib.h"
 
-// #define COMMAND_MAX 128
+#define COMMAND_MAX 128
 
-// int32_t halt (uint8_t status);
+int32_t halt (uint8_t status);
 
-// int32_t execute(const uint8_t *command);
+int32_t execute(const uint8_t *command);
 
-// int32_t read (nt32_t fd, void* buf, nit32_t nbytes);
+int32_t read (int32_t fd, void* buf, int32_t nbytes);
 
-// int32_t write (int32_t fd, const void* buf, int32_t nbytes);
+int32_t write (int32_t fd, const void* buf, int32_t nbytes);
 
-// int32_t open (const uint8_t* filename);
+int32_t open (const uint8_t* filename);
 
-// int32_t close (int32_t fd);
+int32_t close (int32_t fd);
 
-// int32_t getargs (uint8_t* buf, int32_t nbytes);
+int32_t getargs (uint8_t* buf, int32_t nbytes);
 
-// int32_t vidmap (uint8_t** screen_start);
+int32_t vidmap (uint8_t** screen_start);
 
-// int32_t set_handler(int32_t signum, void* handler_address);
+int32_t set_handler(int32_t signum, void* handler_address);
 
-// int32_t sigreturn (void);
+int32_t sigreturn (void);
 
-// #endif
+#endif
