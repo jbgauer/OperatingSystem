4a0136db435f95b112741b11d697c6e759567b08
Blade <bblade2@illinois.edu>
2023-11-01 12:26:57 -0500

    pcb with a couple warnings

--- student-distrib/pcb.c
+++ student-distrib/pcb.c
@@ -1,62 +1,57 @@
-// /* pcb.c - Struct and helper functions for process control block
-//  * 
-//  */
+/* pcb.c - Struct and helper functions for process control block
+ * 
+ */
 
-// #include "pcb.h"
-// #include "paging.h"
-// #include "terminal.h"
+#include "pcb.h"
+#include "paging.h"
+#include "terminal.h"
 
-// /*
-//  * pcb_init
-//  *   DESCRIPTION: initializes a new process control block
-//  *   INPUTS: uint32_t - process number of parent
-//  *   OUTPUTS: changed entry in pcb array
-//  *   RETURN VALUE: none
-//  *   SIDE EFFECTS: loads pcb into array
-//  */
-// void pcb_init(uint32_t par_process_num) {
-//     file_op_t std_file_op;
-//     pcb_t* new_pcb = pcb_array[curr_pid];
-//     new_pcb->stack_ptr = (uint32_t*)(0x800000-(0x2000*process_num)-4);
-//     int j;
-//     new_pcb->in_use = 1;
-//     new_pcb->par_id = par_process_num;
 
-//     new_pcb->inst_ptr = 0;
-//     new_pcb->p_eax = 0;
-//     new_pcb->p_ebx = 0;
-//     new_pcb->p_ecx = 0;
-//     new_pcb->p_edx = 0;
-//     new_pcb->p_edi = 0;
-//     new_pcb->p_esi = 0;
+/*
+ * pcb_init
+ *   DESCRIPTION: initializes a new process control block
+ *   INPUTS: uint32_t - process number of parent
+ *   OUTPUTS: changed entry in pcb array
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: loads pcb into array
+ */
+void pcb_init(uint32_t par_process_num) {
+    file_op_t std_file_op;
+    int curr_proc = 0;
+    int j;
+    pcb_t* new_pcb = &pcb_array[curr_proc];
+    new_pcb->stack_ptr = (uint32_t*)(EIGHT_MB-(EIGHT_KB*curr_proc)-4);
+    new_pcb->base_ptr = (uint32_t*)(EIGHT_MB-(EIGHT_KB*curr_proc)-4);
+    new_pcb->in_use = 1;
+    new_pcb->par_id = par_process_num;
+    new_pcb->inst_ptr = 0;
 
-//     //stdin + stdout vals STILL NEED TO BE CHANGED!
-//     //set fda[0] == stdin
-//     std_file_op.open = &term_open;
-//     std_file_op.read = &term_read;
-//     std_file_op.write = &term_write;
-//     std_file_op.close = &term_close;
-//     new_pcb->fda[0].file_op_ptr = &std_file_op;
-//     new_pcb->fda[0].inode = 0;
-//     new_pcb->fda[0].file_position = 0;
-//     new_pcb->fda[0].flags = 0;
-//     new_pcb->fda[0].filename = "";     //not sure if we want this
-//     new_pcb->fda[0].file_type = 0;     //not sure if we want this
-//     //set fda[1] == stdout 
-//     new_pcb->fda[1].file_op_ptr = &std_file_op;
-//     new_pcb->fda[1].inode = 0;
-//     new_pcb->fda[1].file_position = 0;
-//     new_pcb->fda[1].flags = 0;
-//     new_pcb->fda[1].filename = "";     //not sure if we want this
-//     new_pcb->fda[1].file_type = 0;     //not sure if we want this
+    //set fda[0] == stdin
+    // std_file_op.open = &term_open;
+    // std_file_op.read = &term_read;
+    // std_file_op.write = &term_write;
+    // std_file_op.close = &term_close;
+    new_pcb->fda[0].file_op_ptr = &std_file_op;
+    new_pcb->fda[0].inode = 0;
+    new_pcb->fda[0].file_position = 0;
+    new_pcb->fda[0].flags = 0;
+    //new_pcb->fda[0].filename = "";     //not sure if we want this
+    new_pcb->fda[0].file_type = 0;     //not sure if we want this
+    //set fda[1] == stdout 
+    new_pcb->fda[1].file_op_ptr = &std_file_op;
+    new_pcb->fda[1].inode = 0;
+    new_pcb->fda[1].file_position = 0;
+    new_pcb->fda[1].flags = 0;
+    //new_pcb->fda[1].filename = "";     //not sure if we want this
+    new_pcb->fda[1].file_type = 0;     //not sure if we want this
 
-//     //set rest of array to 0
-//     for(j = 2; j < FILE_MAX; j++) {
-//         new_pcb->fda[j].file_op_ptr = NULL;
-//         new_pcb->fda[j].inode = 0;
-//         new_pcb->fda[j].file_position = 0;
-//         new_pcb->fda[j].flags = 0;
-//         new_pcb->fda[j].filename = "";     //not sure if we want this
-//         new_pcb->fda[j].file_type = 0;     //not sure if we want this
-//     }
-// }
+    //set rest of array to 0
+    for(j = 2; j < FILE_MAX; j++) {
+        new_pcb->fda[j].file_op_ptr = NULL;
+        new_pcb->fda[j].inode = 0;
+        new_pcb->fda[j].file_position = 0;
+        new_pcb->fda[j].flags = 0;
+        //new_pcb->fda[j].filename = "";     //not sure if we want this
+        new_pcb->fda[j].file_type = 0;     //not sure if we want this
+    }
+}
--- student-distrib/pcb.h
+++ student-distrib/pcb.h
@@ -1,57 +1,54 @@
-// /* pcb.h - definitions for struct and helper functions for process control block
-//  * 
-//  */
+/* pcb.h - definitions for struct and helper functions for process control block
+ * 
+ */
 
-// #ifndef _PCB_H
-// #define _PCB_H
+#ifndef _PCB_H
+#define _PCB_H
 
-// #include "lib.h"
-// #include "filesys_driver.h"
-// #define MAX_FILENAME_LEN 32
-// #define NUM_PCB 6
-// #define FILE_MAX 8
+#include "lib.h"
+#include "filesys_driver.h"
+#define MAX_FILENAME_LEN 32
+#define NUM_PCB 6
+#define FILE_MAX 8
+#define EIGHT_MB 0x800000
+#define EIGHT_KB 0x2000
 
-// pcb_t pcb_array[NUM_PCB];
+typedef struct {
+    void (*open)(const uint8_t* filename);
+    void (*read)(int32_t fd, char* buf, int32_t nbytes);
+    void (*write)(int32_t fd, char* buf, int32_t nbytes);
+    void (*close)(int32_t fd);
+} file_op_t;
 
-// typedef struct {
-//     uint32_t in_use;                // 1 if in use, 0 if not in use
-//     uint32_t* stack_ptr;            // Holds current stack position of process
-//     uint32_t par_id;                // parent process number
-//     uint32_t* inst_ptr;             // Holds next instruction to be executed for process
-//     uint32_t p_eax;                 // Holds current registers of process
-//     uint32_t p_ebx;
-//     uint32_t p_ecx;
-//     uint32_t p_edx;
-//     uint32_t p_edi; 
-//     uint32_t p_esi; 
-//     fd_t fda[FILE_MAX];             // Array that holds file descriptors of every file used by this program
-// } pcb_t;
+typedef struct {
+    file_op_t* file_op_ptr;  
+    uint32_t inode;                         //files inode
+    uint32_t file_position;                 //current position in file
+    uint32_t flags;                         //current flags - 1 if in use, 0 if open
+    char filename[MAX_FILENAME_LEN];        //filename
+    uint32_t file_type;                     //type of file
+} fd_t;
 
-// typedef struct {
-//     file_opt_t* file_op_ptr;  
-//     uint32_t inode;                         //files inode
-//     uint32_t file_position;                 //current position in file
-//     uint32_t flags;                         //current flags - 1 if in use, 0 if open
-//     uint8_t filename[MAX_FILENAME_LEN];     //filename
-//     uint32_t file_type;                     //type of file
-// } fd_t;
-
-// typedef struct {
-//     void (*open)(const uint8_t* filename);
-//     void (*read)(int32_t fd, char* buf, int32_t nbytes);
-//     void (*write)(int32_t fd, char* buf, int32_t nbytes);
-//     void (*close)(int32_t fd);
-// } file_op_t;
+typedef struct {
+    uint32_t in_use;                // 1 if in use, 0 if not in use
+    uint32_t* stack_ptr;            // Holds current stack position of process
+    uint32_t* base_ptr;             // Holds current base position of process
+    uint32_t par_id;                // parent process number
+    uint32_t* inst_ptr;             // Holds next instruction to be executed for process
+    fd_t fda[FILE_MAX];             // Array that holds file descriptors of every file used by this program
+} pcb_t;
 
 
-// /*
-//  * pcb_init
-//  *   DESCRIPTION: initializes a new process control block
-//  *   INPUTS: uint32_t - process number of parent
-//  *   OUTPUTS: changed entry in pcb array
-//  *   RETURN VALUE: none
-//  *   SIDE EFFECTS: loads pcb into array
-//  */
-// void pcb_init(uint32_t par_process_num);
+/*
+ * pcb_init
+ *   DESCRIPTION: initializes a new process control block
+ *   INPUTS: uint32_t - process number of parent
+ *   OUTPUTS: changed entry in pcb array
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: loads pcb into array
+ */
+void pcb_init(uint32_t par_process_num);
 
-// #endif
+pcb_t pcb_array[NUM_PCB];
+
+#endif
