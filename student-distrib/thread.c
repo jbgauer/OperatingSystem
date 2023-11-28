60040a22483890c4a6cc2ec53f5a683aa78d5e7c
ece391 <kevinjk4@illinois.edu>
2023-11-28 13:26:38 -0600

    comments for scheduling

--- student-distrib/thread.c
+++ student-distrib/thread.c
@@ -17,4 +17,25 @@ void switch_thread(int thread_id) {
     curr_pid = terminal[thread_id].t_pid;
 
     // Context Switch
+
+
+    // Store contents from TSS to enable restoration of Process state for the next time slice
+
+
+
+    /*
+    Utilize the kernel stack (think about what you did for HALT)
+    You will be using assembly to do the context switch
+    Switch ESP/EBP to next process’ kernel stack
+    Restore next process’ TSS
+    Flush TLB on process switch
+    */
+
+   // to switch to next process:
+   // set up paging, change current process id
+
+   // push current context information to its kernel stack
+
+    // tss
+    // Restore TSS contents relevant to Process and store in pcb
 }
