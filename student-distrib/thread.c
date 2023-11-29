ad842703449d7e75f6981d26fa3f60a552718ab1
ece391 <kevinjk4@illinois.edu>
2023-11-29 13:53:02 -0600

    updates

--- student-distrib/thread.c
+++ student-distrib/thread.c
@@ -15,6 +15,10 @@
  */
 void switch_thread() {
     
+
+    // // not sure if using correct esp, ebp
+    // // not sure when to sent_eoi
+
     // uint32_t pid = terminal[curr_thread].t_pid;
 
     // // spawn shell if pcb is not in use
@@ -49,24 +53,32 @@ void switch_thread() {
     // curr_thread = (curr_thread+1)%3;
     // curr_pid = terminal[curr_thread].t_pid;
 
-    // // Switch ESP/EBP to next process’ kernel stack
-
-
-    // asm volatile(
-    //         "movl %0, %%esp;" 
-    //         "movl %1, %%ebp;" 
-    //         :
-    //         :"r"(pcb_array[curr_pid].stack_ptr), "r"(pcb_array[curr_pid].base_ptr)
-    //         :
-    //         );
+    
 
     // // Paging:
     // // change virtual program image to point to next program in physical mem
     // // redirect the program image
+    // uint32_t newEntry, pageHold; 
+    // pdir_entry_t kerntry;
+
+    // //Set new page (first addr at 0x400)
+    // pageHold = curr_pid;
+    // pageHold += PAGES_DEFAULT_USE;
+    // kerntry.p_addr = pageHold << KENTRY_SHIFT;
+    // kerntry.ps = 1;
+    // kerntry.a = 0;
+    // kerntry.pcd = 0;
+    // kerntry.pwt = 0;
+    // kerntry.us = 1;
+    // kerntry.rw = 1;
+    // kerntry.p = 1;
+    // newEntry = combine_dir_entry(kerntry);
+    // pagedir[USER_SPACE] = newEntry;
     
+    // // Flush TLB on process switch
+    // flush_tlb()
 
-
-
+    
     // // Restore next process’ TSS
 
     // // tss
@@ -75,11 +87,14 @@ void switch_thread() {
     // tss.esp0 = (uint32_t)pcb_array[curr_pid].stack_ptr;
     // tss.ss0  = KERNEL_DS;
 
-    
-
-    // // Flush TLB on process switch
-
-
+    // // Switch ESP/EBP to next process’ kernel stack
+    // asm volatile(
+    //         "movl %0, %%esp;" 
+    //         "movl %1, %%ebp;" 
+    //         :
+    //         :"r"(pcb_array[curr_pid].stack_ptr), "r"(pcb_array[curr_pid].base_ptr)
+    //         :
+    //         );
 
 
 
